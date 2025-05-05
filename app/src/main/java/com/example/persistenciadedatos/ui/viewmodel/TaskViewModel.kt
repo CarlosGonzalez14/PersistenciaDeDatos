43f20766c12087ce31c8d61a35988f223842decc
Carlos Gonzalez <carlos.gonzalezba@anahuac.mx>
2025-05-05 14:24:53 -0600

    Implementación de TaskViewModel.kt

---
+++ app/src/main/java/com/example/persistenciadedatos/ui/viewmodel/TaskViewModel.kt
@@ -0,0 +1,22 @@
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class TaskViewModel(private val repository: TaskRepository) : ViewModel() {
+    val tasks = repository.allTasks
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+    fun addTask(title: String) {
+        viewModelScope.launch {
+            repository.addTask(title)
+        }
+    }
+
+    fun deleteTask(task: Task) {
+        viewModelScope.launch {
+            repository.deleteTask(task)
+        }
+    }
+}
